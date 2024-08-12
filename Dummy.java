8b19d2c7e344f2ee02ec8f1fb94a9b5721ce31f4
yashrai30 <166721479+yashrai30@users.noreply.github.com>
2024-08-12 14:27:22 +0530

    Update Dummy.java

--- Dummy.java
+++ Dummy.java
@@ -1,8 +1,10 @@
 public class Dummy {
 
-    public int add(int a, int b) {
-        return a + b;
-    }
+public int add(int a, int b) {
+    System.out.println("Adding " + a + " and " + b);
+    return a + b;
+}
+
 
     public int subtract(int a, int b) {
         return a - b;
